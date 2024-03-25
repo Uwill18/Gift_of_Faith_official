a80d05fe0670624ce02b43b973039b415a7934a3
Hoshi Holiday <hoshiholiday@gmail.com>
2024-03-25 15:58:20 -0500

    second attempt
    attempt two at adjusting to a responsive navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,44 +11,58 @@ import OutsideClickHandler from 'react-outside-click-handler';
 
 
 const Header = () => {
-  const[menuOpened, setmenuOpened] = useState(false);
+
+  const[isMobile, setIsMobile] = useState(true);
 
   /**if the menu is less than 800px, then 
    * allow the menu to slide 100px off screen
   */
-  const getMenuStyles=(menuOpened)=>{
-    if(document.documentElement.clientWidth <= 800){
-      return {left: !menuOpened && "-100%"}
-    }
 
-  }
   return (
     <nav className="h-wrapper">
-    <div className="flexCenter paddings innerWidth h-container">
-      <span>
+    <div className="flexCenter paddings h-container">
+      
       
     <NavLink to="/" element={<Home/>} className='companyName'>
     <img src="./mariposa.png" alt="logo" width={75}/>
       <strong><em>Gift of Faith Consultants</em></strong>
       </NavLink>
 
-      </span>    
+         
    
       {/* <OutsideClickHandler
       onOutsideClick={()=>{setmenuOpened(false)}}
-      > */}     <div className="menu-icon"
-      onClick={()=>setmenuOpened(!menuOpened)}>
-      <BiMenuAltRight size={30} className='mobile-nav'/>
-    </div>
+      > */}    
+      
 
-      <div className={menuOpened ? "open" : ""}>
-      <div className="flexCenter h-menu">
-        <NavLink to="/" element={<Home/>} className="">Home</NavLink>
-        <NavLink to="/projects" className="mobile-options">Projects</NavLink>
-        <Link to="#team" element={<Team/>} className="mobile-options">Team</Link>
-        <Link to="#about" className="mobile-options">About</Link>
-        <button className="button mobile-options"><Link to="#contact">Contact</Link></button>
-        </div>
+      <div>
+      <ul className={isMobile ? "h-menu-mobile" : "flexCenter h-menu"}>
+        <NavLink to="/" element={<Home/>} className="mobile-options">
+          <li>Home</li>
+          </NavLink>
+        <NavLink to="/projects" className="mobile-options">
+          <li>Projects</li>
+          </NavLink>
+        <Link to="#team" element={<Team/>} className="mobile-options">
+          <li>Team</li>
+          </Link>
+        <Link to="#about" className="mobile-options">
+          <li>About</li>
+          </Link>
+        
+          <Link to="#contact">
+          <button className="mobile-options button ">
+            <li>Contact</li>
+            </button>
+          </Link>
+          
+        </ul>
+        <button className="menu-icon"
+        onClick={()=>setIsMobile(!isMobile)}>
+        {isMobile ? (<i className='fas fa-times'></i>) 
+        : (<i className='fas fa-bars'></i>)  }
+      {/* <BiMenuAltRight size={30} className='mobile-nav'/> */}
+    </button>
     
       </div>
       {/* </OutsideClickHandler> */}
