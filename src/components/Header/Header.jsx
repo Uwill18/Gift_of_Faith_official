aa51995a156e54a57022140ad31545ba924d0623
Hoshi Holiday <hoshiholiday@gmail.com>
2024-03-27 04:33:19 -0500

    nearly finished with mobile
    mobile nearly done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,21 +13,20 @@ import { Helmet } from 'react-helmet';
 
 const Header = () => {
 
-  const[isMobile, setIsMobile] = useState(false);
+  const[isMobile, setIsMobile] = useState(true);
 
-  /**if the menu is less than 800px, then 
-   * allow the menu to slide 100px off screen
-  */
+
 
   return (
     <nav className="h-wrapper">
     <div className="flexCenter paddings h-container">
       
-      
+    
     <NavLink to="/" element={<Home/>} className='companyName'>
     <img src="./mariposa.png" alt="logo" width={75}/>
       <strong><em>Gift of Faith Consultants</em></strong>
       </NavLink>
+    
 
          
    
@@ -37,40 +36,36 @@ const Header = () => {
       
 
       <div>
-      <ul className={isMobile ? "h-menu-mobile" : "h-menu"} id="menuList">
+      
+      <div className={isMobile ? "h-menu-mobile"   :  "flexCenter h-menu-mobile" } id="menuList">
+        {/* <ul className={isMobile ? "h-menu-mobile"   :  "flexCenter h-menu" } id="menuList"> */}
         <NavLink to="/" element={<Home/>} >
-          <li className="mobile-options">Home</li>
+          <li className="h-menu-mobile">Home</li>
           </NavLink>
         <NavLink to="/projects" >
-          <li className="mobile-options">Projects</li>
+          <li className="h-menu-mobile">Projects</li>
           </NavLink>
         <Link to="#team" element={<Team/>} >
-          <li className="mobile-options">Team</li>
+          <li className="h-menu-mobile">Team</li>
           </Link>
         <Link to="#about" >
-          <li className="mobile-options">About</li>
+          <li className="h-menu-mobile">About</li>
           </Link>
         
           <Link to="#contact">
-          <li className="mobile-options">
+          <li className="h-menu-mobile">
           <button className="button">
             Contact
             </button>
             </li>
           </Link>
           
-        </ul>
-        <div className="menu-icon">
+        </div>
+        {/* <div className="menu-icon">
           <i className="fa-solid fa-bars" onClick={()=>toggleMenu()}>
           </i>
-        </div>
+        </div> */}
         
-
-             {/* <button className="menu-icon"
-        onClick={()=>setIsMobile(!isMobile)}>
-        {isMobile ? (<i className='fas fa-times'></i>) 
-        : (<i className='fa-solid fa-bars'></i>)  }
-        </button> */}
 
        
       {/* <BiMenuAltRight size={30} className='mobile-nav'/> */}
@@ -78,6 +73,12 @@ const Header = () => {
     
       </div>
       {/* </OutsideClickHandler> */}
+
+      <button className="menu-icon"
+        onClick={()=>setIsMobile(!isMobile)}>
+        {isMobile ? (<i className='fas fa-times'></i>) 
+        : (<i className='fa-solid fa-bars'></i>)  }
+        </button>
     </div>
    
 
